40c5f7b7965e511179a8075156ad46a354d47f8b
MOKSHAGNA TEJ KALEPALLI <mokshagnatejkalepalli@gmail.com>
2026-02-11 08:52:55 +0530

    commit

--- Task-2/IDENTIFYSMITHNUMBER.cpp
+++ Task-2/IDENTIFYSMITHNUMBER.cpp
@@ -1,6 +1,6 @@
 int sum_of_digits(int num) {
  
-// Function to calculate the sum of the digits of a number
+
 int sum = 0;
 while (num > 0) {
    sum += num % 10;
@@ -10,11 +10,11 @@ return sum;
 }
 int sum_of_prime_factors_digits(int num) {
  
-// Function to calculate the sum of the digits of prime factors
+
 int sum = 0;
 int factor = 2;
 while (num > 1) {
-   // Calculate prime factors and sum their digits
+  
    while (num % factor == 0) {
        sum += sum_of_digits(factor);
        num /= factor;
@@ -24,7 +24,7 @@ while (num > 1) {
        break;
    }
 }
-// Add the digits of any remaining prime factor greater than 1
+
 if (num > 1) {
    sum += sum_of_digits(num);
 }
@@ -36,17 +36,17 @@ int solve(int n) {
    return 0;
 }
  
-// Calculate the sum of the digits of the integer
+
 int sum_num_digits = sum_of_digits(n);
  
-// Calculate the sum of the digits of prime factors
+
 int sum_factors_digits = sum_of_prime_factors_digits(n);
  
-// Return 1 if the sums are equal, otherwise return 0
+
 if (sum_num_digits == sum_factors_digits) {
-   return 1; // n is a Smith number
+   return 1; 
 } else {
-   return 0; // n is not a Smith number
+   return 0; 
 }
  
 }
